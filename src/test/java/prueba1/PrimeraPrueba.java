e8c9c25ba4c78ee81aafa5483366d280c0fa9ca6
duban.rojas <d.andresrojas.h@gmail.com>
2020-09-14 10:11:11 -0500

    Api-Rest

---
+++ src/test/java/prueba1/PrimeraPrueba.java
@@ -0,0 +1,12 @@
+package prueba1;
+
+import org.testng.annotations.Test;
+
+public class PrimeraPrueba {
+
+    @Test
+    public void testng() {
+        System.out.println("Welcome to curse of API");
+    }
+}
+
